0e3ad72317c7cb3482168373f3c4fca69a18f64e
Rana Ahmed <rana.ahmad+github@hotmail.com>
2022-09-09 16:11:41 +0000

    Set

--- src/main/java/com/mkyong/hashing/App.java
+++ src/main/java/com/mkyong/hashing/App.java
@@ -1,5 +1,8 @@
 package com.mkyong.hashing;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Map<String, String> m = new HashMap<>();
+        m.put("A", "1");
+        m.put("B", "2");
+        System.out.print(m);
     }
 }
